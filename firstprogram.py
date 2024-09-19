e28b2a3227235b92e00d4632da52bb8e5cde1911
varvaraBo8 <varvara_kim@icloud.com>
2024-09-19 09:36:04 -0400

    BAZINGA I MADE A CALCLATOR

--- firstprogram.py
+++ firstprogram.py
@@ -1,1 +1,9 @@
-print("Hello, world!")
+# calculater
+
+print("whatsup my freaks, this is a sigma calculator")
+num1 = int(input("Enter a number: "))
+num2 = int (input("Enter another number: "))
+
+answer = num1 + num2
+
+print(answer)
